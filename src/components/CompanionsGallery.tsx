89c8e73cbceebd68b044ff3381142ff3db608a96
Higor Felipe Ribeiro <higorfeliperibeiro67@gmail.com>
2025-06-13 18:57:48 -0300

    Update CompanionsGallery.tsx

--- src/components/CompanionsGallery.tsx
+++ src/components/CompanionsGallery.tsx
@@ -53,7 +53,7 @@ export default function CompanionsGallery({ language }: CompanionsGalleryProps) 
   useEffect(() => {
     async function fetchData() {
       try {
-        const res = await fetch(`https://usable-crown-91e30743ba.strapiapp.com/api/companions?locale=${language}`);
+        const res = await fetch(`https://a20b-2a02-4780-14-b15c-00-1.ngrok-free.app/api/companions?locale=${language}`);
 
         const json = await res.json();
         type ApiCompanion = {
@@ -91,7 +91,7 @@ export default function CompanionsGallery({ language }: CompanionsGalleryProps) 
     // ✅ Buscar título, subtítulo e botão de "View All"
     async function fetchGalleryContent() {
       try {
-        const res = await fetch(`https://usable-crown-91e30743ba.strapiapp.com/api/companions-gallery-content?locale=${language}`);
+        const res = await fetch(`https://a20b-2a02-4780-14-b15c-00-1.ngrok-free.app/api/companions-gallery-content?locale=${language}`);
 
         const json = await res.json();
         const data = json.data;
