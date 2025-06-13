c2abb7bef433a9932b51bf39ac632ada3ebdddfb
Higor Felipe Ribeiro <higorfeliperibeiro67@gmail.com>
2025-06-13 19:23:26 -0300

    Update HowItWorks.tsx

--- src/components/HowItWorks.tsx
+++ src/components/HowItWorks.tsx
@@ -36,7 +36,7 @@ export default function HowItWorksSection({ language }: WhyChooseUsSectionProps)
   const [button, setButton] = useState({ text: "", icon: "" });
 
   useEffect(() => {
-    fetch(`https://a20b-2a02-4780-14-b15c-00-1.ngrok-free.app/api/why-choose-content?populate[cards]=true&populate[bottomButton]=true&locale=${language}`)
+    fetch(`https://kxcjd596-1337.brs.devtunnels.ms/api/why-choose-content?populate[cards]=true&populate[bottomButton]=true&locale=${language}`)
 
       .then((res) => res.json())
       .then((json) => {
@@ -102,7 +102,7 @@ export default function HowItWorksSection({ language }: WhyChooseUsSectionProps)
   }, [language]);
   
   useEffect(() => {
-    fetch(`https://a20b-2a02-4780-14-b15c-00-1.ngrok-free.app/api/why-choose-section?populate[bottomCards]=true&locale=${language}`)
+    fetch(`https://kxcjd596-1337.brs.devtunnels.ms/api/why-choose-section?populate[bottomCards]=true&locale=${language}`)
 
       .then((res) => res.json())
       .then((json) => {
